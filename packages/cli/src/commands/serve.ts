0b93bfcf45c62a389005b222eb9a19444bfa06f1
0x-cygnet <bob@cygnetops.com>
2022-09-02 13:25:57 -0500

    2

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -11,7 +11,7 @@ const isProduction = process.env.NODE_ENV === "production";
 export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
-  .option("-p, --port <number>", "port to run server on", "4005")
+  .option("-p, --port <number>", "Port to run server on", "4005")
   .action(async (filename = "notebook.js", options: { port: string }) => {
     const isLocalApiError = (err: any): err is LocalApiError => {
       return typeof err.code === "string";
